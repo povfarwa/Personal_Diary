44debaa2c8a397d3b24bc7ab05279ba8cd80fdd9
Farwa Zafar <pov.farwa@gmail.com>
2026-06-11 18:23:07 +0500

    write basic code of express and require important things and make server.js folder

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require ('express')
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+const cookieParser = require('cookie-parser')
+const app = express()
+
+app.use(express.json({ extended: true }))
+app.use(express.json)
+app.use(cookiePrser)
+app.set('view engine', 'ejs')
+
