a46c26d83e82badb1755e216e3b9a6fae86184f6
Farwa Zafar <pov.farwa@gmail.com>
2026-06-14 12:12:44 +0500

    make routers of register and login page in express in server.js

--- server.js
+++ server.js
@@ -10,5 +10,39 @@ app.use(cookiePrser)
 app.set('view engine', 'ejs')
 
 app.get('/register', (req, res) => {
-    res.render('/register')
+    res.render('register', {error: null})
+})
+
+app.post('/register', async (req, res) => {
+    const { usernme, password } = req.body
+    try {
+        let user = await bcrypt.hash(password, 10);
+        if(user) return res.render('register', { error: "Username alreadytaken"})
+        const hashedPassword = await bcrypt.hash(password, 10);
+        user = new user({ username, password: hashedPassword })
+        await user.save()
+        res.redirect('/login')
+    } catch (err) {
+        res.status(500).send('Server Error')
+    }
+});
+
+app.get('/login', (req, res) => {
+    res.render('login', {error: null})
+})
+
+pp.post('/login', async (req, res) => {
+    const { username, password } = req.body
+    try{
+        const user = await UserActivation.findOne({ username })
+        if(!user) return res.render('login', { error: 'Invalid User'})
+        const isMatch = await bcrypt.compare(password, user.password);
+        if(!isMatch) return res.render('login', { error: 'Invalid Creadentials'})
+        
+        const token = jwt.sign({ id: user._id }), process.env.JWT_SECRET, { expiresIn: '1h'};
+        res.cookie('token', token, { httpOnly: true })
+        res.redirect('/dashboard');
+    } catch (err) {
+        res.status(500).send('Server Error')
+    }
 })
